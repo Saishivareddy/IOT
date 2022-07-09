a2d76a895ca4ba5558764141d1c0ea6ef23ffbcf
Saishivareddy <saishivareddy75@gmail.com>
2022-07-09 21:56:38 +0530

    ESP32 acting as Access Point and also Station

--- WIFI_AP_STA/WIFI_AP_STA.ino
+++ WIFI_AP_STA/WIFI_AP_STA.ino
@@ -9,6 +9,7 @@ void setup() {
   // put your setup code here, to run once:
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ssid1, passwd1);
+  Serial.begin(9600);
   Serial.print("Network Created with ssid :");
   Serial.println(ssid);
   Serial.println("Network is available to use");
