7f6f43558b286c7d663eb2ead26c4f0293bfd8c3
Saishivareddy <saishivareddy75@gmail.com>
2022-07-10 20:09:17 +0530

    Distance Using Ultrasonic Sensor and ThingSpeak

---
+++ Ultrasonic_Thingspeak/Ultrasonic_Thingspeak.ino
@@ -0,0 +1,59 @@
+#include <ThingSpeak.h>
+#include <WiFi.h>
+
+#define trigpin 26
+#define echopin 27
+#define led 13
+float distance = 0;
+int duration = 0;
+
+WiFiClient client;
+char ssid[] = "Shiva";
+char pass[] = "shiva@1412";
+
+long channel = 1795117;
+int channelfield = 1;
+char auth[] = "JAZ57NN98P3CEZ1L";
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  ThingSpeak.begin(client);
+  pinMode(trigpin, OUTPUT);
+  pinMode(echopin, INPUT);
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  WiFi.begin(ssid,pass);
+  if(WiFi.status() == WL_CONNECTED)
+  {
+    Serial.print("Connected to ssid :");
+    Serial.println(ssid);
+  }
+  digitalWrite(trigpin,LOW);
+  delay(1);
+  digitalWrite(trigpin, HIGH);
+  delay(10);
+  digitalWrite(trigpin, LOW);
+
+  duration = pulseIn(echopin, HIGH);
+  distance = (duration*0.034)/2;
+  Serial.print("Distance :");
+  Serial.print(distance);
+  Serial.println(" cm");
+  
+  if(distance <= 15)
+  {
+    digitalWrite(led, HIGH);
+  }
+  else
+  {
+    digitalWrite(led, LOW);
+  }
+  ThingSpeak.writeField(channel, channelfield, distance, auth);
+  delay(2000);
+}
