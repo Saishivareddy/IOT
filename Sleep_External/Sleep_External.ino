42c5d978b2d384fb2ea029af0750a85477ccb132
Saishivareddy <saishivareddy75@gmail.com>
2022-07-15 01:47:13 +0530

    Waking up Using EXT0

--- Sleep_External/Sleep_External.ino
+++ Sleep_External/Sleep_External.ino
@@ -2,20 +2,20 @@
 
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
-delay(1000);
-pinMode(led, OUTPUT);
-Serial.println("Hello World");
+  Serial.begin(9600);
+  delay(1000);
+  pinMode(led, OUTPUT);
+  Serial.println("Hello World");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(led, HIGH);
-delay(1000);
-digitalWrite(led, LOW);
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
 
-esp_sleep_enable_ext0_wakeup(GPIO_NUM_36,1);
-Serial.println("Going to sleep");
-delay(500);
-esp_light_sleep_start();
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_36, 1);
+  Serial.println("Going to sleep");
+  delay(500);
+  esp_light_sleep_start();
 }
