a9fd9bea132ad2457f713317a49788cc9bbee8ce
Saishivareddy <saishivareddy75@gmail.com>
2022-07-17 23:05:07 +0530

    Controlling LED using the LDR Value

--- LDR_LED/LDR_LED.ino
+++ LDR_LED/LDR_LED.ino
@@ -1,25 +1,26 @@
 int led = 12;
-int ldr = 13; 
+int ldr = 13;
 void setup() {
   // put your setup code here, to run once:
-pinMode(led, OUTPUT);
-pinMode(ldr, INPUT);
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  pinMode(ldr, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int val = analogRead(ldr);
-
-  if(val <= 511)
+  int x = map(val,0, 4095, 0, 10);
+  if (x <= 2)
   {
     digitalWrite(led, HIGH);
-    Serial.println(val);
+    Serial.println(x);
     Serial.println("Light is Low and LED is ON");
   }
   else
   {
     digitalWrite(led, LOW);
-    Serial.println(val);
+    Serial.println(x);
     Serial.println("Light is More and LED is OFF");
   }
   delay(500);
